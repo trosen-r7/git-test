e028aca95e0efe3ad70614aceb5db30b4d8cae36
Trevor Rosen <trevor@catapult-creative.com>
2011-11-02 09:44:01 -0500

    * Added some dumb behavior

---
+++ badness.rb
@@ -0,0 +1,14 @@
+class Badness
+  attr_accessor :herp, :derp
+  attr_reader :random_number
+  
+  def initialize(herp, derp)
+    @herp, @derp = herp, derp
+    @random_number = rand(50)
+  end
+
+  def to_s
+    "At #{Time.now}, we shall #{herp} in order to #{derp}"
+  end
+
+end
